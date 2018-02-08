c5c11d58b5525c79540068257ff412bf6f1341b5
mchgbnath <35652847+mchgbnath@users.noreply.github.com>
2018-02-08 16:36:24 +0530

    Create Power of Number.c

---
+++ Power of Number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter The Number\n");
+    scanf("%d",&a);
+    printf("enter the power\n");
+    scanf("%d",&b);
+    c=pow(a,b);
+    printf("C%d",c);
+}
+
